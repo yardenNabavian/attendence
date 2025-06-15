6bd26aafee650a90f1685a336280baf08a093527
Yarden Nabavian <ynabavian@gmail.com>
2025-06-15 14:10:53 +0300

    swagger in prod

--- AttendenceApi/Program.cs
+++ AttendenceApi/Program.cs
@@ -18,11 +18,8 @@ builder.Services.AddSingleton<AttendenceApi.Company>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 // Enable CORS
 app.UseCors("AllowAll");
